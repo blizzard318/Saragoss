033f1575f89aa3cf80de50b9533992a6ef161269
Andrew Chen <anew318@hotmail.com>
2024-12-16 17:41:06 +0800

    hmm

---
+++ Website/sort.js
@@ -0,0 +1,18 @@
+function init() {
+    if (!sessionStorage.getItem('darkmode') && window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches)
+        sessionStorage.setItem('darkmode', true);
+
+    SetDarkMode(sessionStorage.getItem('darkmode') === 'true');
+    document.getElementById('darkmode').checked = (sessionStorage.getItem('darkmode') === 'true');
+    document.getElementById('darkmode').addEventListener('change', () => SetDarkMode(document.getElementById('darkmode').checked));
+
+    function SetDarkMode(setval) {
+        if (setval) document.querySelector('html').dataset.darkmode = true;
+        else delete document.querySelector('html').dataset.darkmode;
+
+        sessionStorage.setItem('darkmode', setval);
+    }
+
+    const tables = document.querySelectorAll('table');
+    tables.forEach(table => colorCode(table));
+}
