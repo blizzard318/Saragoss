f0068361b2859fbfc60139d7d688ff72d492acf5
Andrew Chen <anew318@hotmail.com>
2024-12-24 11:45:29 +0800

    try

--- Website/Login/register.js
+++ Website/Login/register.js
@@ -1,17 +1,17 @@
-  async function register() {
-    const password = document.getElementById('reg-pw').value;
-    const rpassword = document.getElementById('reg-rpw').value;
-	
+async function register() {
+	const password = document.getElementById('reg-pw').value;
+	const rpassword = document.getElementById('reg-rpw').value;
+
 	document.getElementById('pw-match').style.display = (password === rpassword) ? "none" : "block";
-	
-    const email = document.getElementById('reg-email').value;
 
-    const response = await fetch('/register', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+	const email = document.getElementById('reg-email').value;
 
-    const result = await response.json();
-    console.log(result);  // Handle the response (e.g., success or error message)
-  }
+	const response = await fetch('/register', {
+	  method: 'POST',
+	  body: JSON.stringify({ email, password }),
+	  headers: { 'Content-Type': 'application/json' },
+	});
+
+	const result = await response.json();
+	console.log(result);  // Handle the response (e.g., success or error message)
+}
