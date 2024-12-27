9e71ff833460c39f1c66a9df3fc943fec80ab9f7
Andrew Chen <anew318@hotmail.com>
2024-12-27 10:32:30 +0800

    hmm?

--- Website/functions/register.js
+++ Website/functions/register.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = context.env.SUPABASE_URL;
 const supabaseKey = context.env.SUPABASE_PRIVATE_KEY;
