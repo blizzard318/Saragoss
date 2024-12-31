eef42101af4e15c72e80b2ac59371ccf94b49d89
blizzard318 <anew318@hotmail.com>
2025-01-01 00:34:55 +0800

    Update login.js

--- Website/Login/login.js
+++ Website/Login/login.js
@@ -31,7 +31,7 @@ async function login() {
 			
 			const result = await response.json();
 			console.log(result);  // Handle the response (e.g., success or error message)
-			
+			sessionStorage.putItem("uuid", result.uuid);
 			window.location.href = result.CreateCharacter ? "../CreateCharacter" : "../Game";
 			break;
 	}
@@ -61,6 +61,7 @@ async function register() {
 				break;
 		}
 		const result = await response.json();
+sessionStorage.putItem("uuid", result.uuid);
 		console.log(result);  // Handle the response (e.g., success or error message)
 	}
 }
