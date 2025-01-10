ead8788ee3ffc552617a3245a231cb2fbf083de8
blizzard318 <anew318@outlook.sg>
2025-01-10 14:47:52 +0800

    Update TurnOver.js

--- TurnOver.js
+++ TurnOver.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 //import jwt from "@tsndr/cloudflare-worker-jwt";
 import jwt from "jsonwebtoken";
 
@@ -36,6 +37,8 @@ fs.appendFile(process.env.GITHUB_ENV, `KV_ID=${KV_ID}`, (err) => {
 
 const weatherOptions = ["Cold", "Foggy", "Hot", "Stormy", "Windy", "Calm"];
 const weatherData = weatherOptions[Math.floor(Math.random() * weatherOptions.length)];
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 const filePath = path.join(__dirname, "Website/weather.txt");
 
 // Ensure the directory exists
