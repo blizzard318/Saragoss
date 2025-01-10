4bd94d75c5305b12addf98d8ce1286fce1843748
blizzard318 <anew318@outlook.sg>
2025-01-10 14:23:06 +0800

    Update GithubAction.js

--- Website/functions/API/GithubAction.js
+++ Website/functions/API/GithubAction.js
@@ -4,17 +4,17 @@ import jwt from '@tsndr/cloudflare-worker-jwt';
 export async function onRequestGet(context) {  
 	const authorization = context.request.headers.get('Authorization');
 	if (!authorization) {
-		return new Response(JSON.stringify(msg: 'Authorization header is missing'), { status: 401 });
+		return new Response(JSON.stringify({ msg: 'Authorization header is missing' }), { status: 401 });
 	}
 	
 	const token = authorization.split(' ')[1];
 	if (!token) {
-		return new Response(JSON.stringify(msg: 'Token is missing'), { status: 401 });
+		return new Response(JSON.stringify({ msg: 'Token is missing' }), { status: 401 });
 	}
 	
 	const verifiedToken = await jwt.verify(token, context.env.JWT_SECRET, { clockTolerance: 60 });
 	if (!verifiedToken) {
-		return new Response(JSON.stringify(msg: 'Token is wrong'), { status: 401 });
+		return new Response(JSON.stringify({ msg: 'Token is wrong' }), { status: 401 });
 	}
 
 	const characters = await context.env.Characters.list();
