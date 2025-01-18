382039551133dab3143e8089881704c5eca81d30
blizzard318 <anew318@outlook.sg>
2025-01-18 14:04:09 +0800

    Update action.js

--- Website/functions/API/action.js
+++ Website/functions/API/action.js
@@ -1,1 +1,36 @@
-test
+import { createClient } from '@supabase/supabase-js';
+
+export async function onRequestPut(context) { //Submit action
+  const supabaseUrl = context.env.SUPABASE_URL;
+  const supabaseKey = context.env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    return new Response(error.message, { status: 401 });
+  }
+
+const { action } = await context.request.json();
+  if (!action ) return new Response("Missing Action", { status: 400 });
+
+  const ship = await ctx.env.keyvalur.get(data.user.id);
+if (!ship) return new Response("Missing Ship", { status: 400 });
+
+const query = `
+        INSERT INTO YourTableName (UUID, Ship, Action)
+        VALUES (?, ?, ?)
+        ON CONFLICT(UUID) DO UPDATE SET
+          Ship = excluded.Ship,
+          Action = excluded.Action
+      `;
+      const params = [uuid, ship, action];
+
+      const result = await env.DB.prepare(query).bind(...params).run();
+
+  
+
+  return new Response(JSON.stringify({ message: 'Create Successful' }), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 200,
+  });
+}
