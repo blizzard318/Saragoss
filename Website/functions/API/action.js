8403bd53810275ec0d1472835f1432965915a131
Andrew Chen <anew318@hotmail.com>
2025-05-20 10:42:33 +0800

    fix?

--- Website/functions/API/action.js
+++ Website/functions/API/action.js
@@ -10,24 +10,22 @@ export async function onRequestPut(context) { //Submit action
     return new Response(error.message, { status: 401 });
   }
 
-const { action } = await context.request.json();
+  const { action } = await context.request.json();
   if (!action ) return new Response("Missing Action", { status: 400 });
 
-  const data = await ctx.env.Characters.get(data.user.id);
-  const ship = data.split('/')[1];
+  const chardata = await ctx.env.Characters.get(data.user.id);
+  const ship = chardata.split('/')[1];
 
-const query = `
+  const query = `
         INSERT INTO YourTableName (UUID, Ship, Action)
         VALUES (?, ?, ?)
         ON CONFLICT(UUID) DO UPDATE SET
           Ship = excluded.Ship,
           Action = excluded.Action
       `;
-      const params = [uuid, ship, action];
+  const params = [uuid, ship, action];
 
-      const result = await env.database.prepare(query).bind(...params).run();
-
-  
+  const result = await env.database.prepare(query).bind(...params).run();
 
   return new Response(JSON.stringify({ message: 'Create Successful' }), {
     headers: { 'Content-Type': 'application/json' },
